6fd1f2b5c33f276772b4e6442367136a2cbfae82
ileydaburnaz <ileydaburnaz2021@gmail.com>
2021-12-13 19:25:05 +0300

    orders list

---
+++ src/Product.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+import { View, Text, FlatList } from "react-native";
+import { Card } from "react-native-elements";
+
+const Product = () => {
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    fetch("https://northwind.vercel.app/api/orders")
+      .then((res) => res.json())
+      .then((data) => {
+        setProduct(data);
+      });
+  }, []);
+  return (
+    <>
+      <View>
+        {product &&
+          product.map((item, key) => (
+            <Card key={key}>
+              <Card.Title>{item.shipName}</Card.Title>
+            </Card>
+          ))}
+      </View>
+    </>
+  );
+};
+
+export default Product;
